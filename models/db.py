151376ef27126b6ff6d96d80171690310fcf1fc6
ricardorub <65383995+ricardorub@users.noreply.github.com>
2025-09-16 16:18:47 -0500

    Delete models directory

--- models/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host='localhost',
-        user='root',      # Cambia por tu usuario MySQL
-        password='root',  # Cambia por tu contraseña MySQL
-        database='tesis4_db'
-    )
