d0f8edd643bb64d086deaf953b26a1b34f4cd7f2
Rubricardo <clicperu@gmail.com>
2026-03-17 21:26:01 -0500

    rev

--- create_tables.py
+++ create_tables.py
@@ -4,7 +4,7 @@ from models.db import db
 # The models need to be imported so that SQLAlchemy knows about them
 from models.model import User, Message
 
-with app.app_context():
-    db.create_all()
+#with app.app_context():
+#    db.create_all()
 
 print("Database tables created successfully.")
